10f91dc38be6ccfe692f64a8be8238af7f2b5679
xiaoyueyue165 <1656800216@qq.com>
2020-01-12 00:42:23 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # rainbow-vue-cli
+
 Vue CLI
+
+## Building and running on localhost
+
+First install dependencies:
+
+```sh
+npm install
+```
+
+To create a production build:
+
+```sh
+npm run build-prod
+```
+
+To create a development build:
+
+```sh
+npm run build-dev
+```
+
+## Running
+
+```sh
+node dist/bundle.js
+```
+
+## Credits
+
+Made with [createapp.dev](https://createapp.dev/)
---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const webpack = require('webpack');
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+const resolve = dir => path.resolve(__dirname, dir);
+
+const config = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader?cacheDirectory=true',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(scss)$/,
+        use: ['vue-style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.vue'],
+    alias: {
+      '@': resolve('src'), // 这样配置后 @ 可以指向 src 目录
+    },
+  },
+  devServer: {},
+  devtool: '#eval-source-map',
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: require('html-webpack-template'),
+      inject: false,
+      appMountId: 'app',
+      meta: [
+        {
+          name: 'description',
+          content: 'A better default template for rainbow-vue-cli.',
+        },
+      ],
+    }),
+    new LodashModuleReplacementPlugin(),
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\\/]node_modules[\\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+};
+
+module.exports = config;
